66b83efd2fab84a30823fed928db3e232f7dee97
BorodinArseniy <113860966+BorodinArseniy@users.noreply.github.com>
2023-01-08 21:49:52 +0300

    Update ProductList.java

--- src/recipe/ProductList.java
+++ src/recipe/ProductList.java
@@ -8,6 +8,7 @@ import java.util.Set;
 public class ProductList {
 
 
+    private Set<Product> products = new HashSet<>();
     private Map<Product, Integer> products = new HashMap<>();
 
     public void addProduct(Product product, Integer count){
